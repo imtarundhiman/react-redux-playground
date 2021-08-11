d01c7f4b2f349afa650e698ecabca18c8577f522
Tarun Dhiman <tarun.dhiman@eisenvault.com>
2021-08-11 16:36:51 +0530

    changes

--- src/store.js
+++ src/store.js
@@ -5,6 +5,6 @@ import counterReducer from './reducers/counterReducers'
 const store = createStore(combineReducers({
     counter: counterReducer,
     profile: profileReducer
-}))
+}), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 export default store
