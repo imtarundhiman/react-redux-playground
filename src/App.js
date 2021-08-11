7bf5a5ff303df539fb8e5ded6fa120e13a276cfc
Tarun Dhiman <tarun.dhiman@eisenvault.com>
2021-08-11 16:10:08 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
 import React from 'react';
-import store from './store'
 import {incrementActionGenerator, decrementActionGenerator} from './actions/counterActions'
-import {saveDetailsActionGenerator} from './actions/profileActions'
+import { connect } from 'react-redux';
 
 class App extends React.Component {
-  componentDidMount(){
-    
-    // can see when anything changes in store
-    store.subscribe(() => {
-      console.log(store.getState())
-    })
 
-    // dispatch actions
-    // we can pass an action obj or an action generator method
-    store.dispatch(incrementActionGenerator({
-      incrementBy: 6
-    }))
-
-    store.dispatch(decrementActionGenerator({
-      decrementBy: 2
-    }))
-
-    store.dispatch(saveDetailsActionGenerator({
-      name: 'Tarun',
-      email: 'email@abc.com',
-      mobile: '7485968574'
-    }))
+  constructor(props){
+    super(props)
   }
 
   render(){
     return <>
-      Hey
+      count : {this.props.counter?.count}
+      <button onClick={() => this.props.dispatch(decrementActionGenerator())}>-</button>
+      <button onClick={() => this.props.dispatch(incrementActionGenerator())}>+</button>
     </>
   }
 }
 
-export default App;
+export default connect((state, ownProps) => {
+  return {
+    counter: state.counter
+  }
+})(App);
